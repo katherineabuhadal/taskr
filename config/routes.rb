7edf5167dbbc220ac549b4fe49a82d9da530fbe7
katherineabuhadal <katherine8@verizon.net>
2014-07-08 11:06:36 -0400

    users can sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  root 'tasks#index'
+  get "/sign_up" => "users#new", as: :sign_up
+  get "/sign_in" => "sessions#new", as: :sign_in
+  
+  resources :tasks, only: [:create, :index]  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
